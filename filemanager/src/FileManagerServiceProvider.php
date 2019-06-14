2d5a9add4caf6516151c38ebf33fa33f6b483050
MasterInformatic <51809180+MasterInformatic@users.noreply.github.com>
2019-06-13 22:00:31 -0500

    Add files via upload

---
+++ filemanager/src/FileManagerServiceProvider.php
@@ -0,0 +1,13 @@
+   <?php
+    // MyVendor\contactform\src\ContactFormServiceProvider.php
+    namespace vendor\fileManager;
+    use Illuminate\Support\ServiceProvider;
+
+    class FileManagerServiceProvider extends ServiceProvider {
+        public function boot()
+        {
+        }
+        public function register()
+        {
+        }
+    }
